5733411986c5192262034fc3bc39920bd5c2afd8
Howard <newtonguass@gmail.com>
2022-02-05 23:09:12 +0800

    add worker node joining

--- start.sh
+++ start.sh
@@ -1,10 +1,11 @@
 #/bin/bash
 cd terraform
-#terraform init terraform plan -out=.tfoutput
-#terraform apply .tfoutput
-#mkdir -p ../.credential
-#terraform output -raw tls_private_key > ../.credential/id_rsa
-#chmod 600 ../.credential/id_rsa
+terraform init 
+terraform plan -out=.tfoutput
+terraform apply .tfoutput
+mkdir -p ../.credential
+terraform output -raw tls_private_key > ../.credential/id_rsa
+chmod 600 ../.credential/id_rsa
 k8sMaster_pub_ip=$(terraform output  -json public_ip_address| jq '.[0]' ) 
 k8sWorker_pub_ip=$(terraform output  -json public_ip_address| jq '.[1]' ) 
 cd ../ansible
